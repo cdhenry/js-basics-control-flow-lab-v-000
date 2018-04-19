ceb249af6bcbec09817818e34d32b2c1db91207b
Chris Henry <cdhenry@gmail.com>
2018-04-19 18:19:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
-
+  'This one is on me!'
+  'I will gladly take your thirty bucks.'
 }
 
 function ternaryCheckCity(someValue){
