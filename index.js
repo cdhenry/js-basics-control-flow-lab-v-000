2727705acd071e67dcb43259fbedc78c47bdf2a6
Chris Henry <cdhenry@gmail.com>
2018-04-19 18:24:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 // Write your code in this file!
 function scuberGreetingForFeet(someValue) {
+  let response;
   if (someValue <= 400) {
-    'This one is on me!'
+    response = 'This one is on me!';
   } else if (someValue > 2000 && someValue < 2500) {
-    'I will gladly take your thirty bucks.'
+    response = 'I will gladly take your thirty bucks.';
   } else if (someValue > 2500) {
-    'No can do.'
+    respons = 'No can do.';
   }
+  return response;
 }
 
 function ternaryCheckCity(someValue){
