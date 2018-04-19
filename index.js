044b2e222909952ab87bb74f04faf8c5e084a0b7
Chris Henry <cdhenry@gmail.com>
2018-04-19 18:23:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 function scuberGreetingForFeet(someValue) {
   if (someValue <= 400) {
     'This one is on me!'
-  } else if () {
+  } else if (someValue > 2000 && someValue < 2500) {
     'I will gladly take your thirty bucks.'
-  } else {
+  } else if (someValue > 2500) {
     'No can do.'
   }
 }
